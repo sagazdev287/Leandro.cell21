4eee78772ce27933389c783ec4ef5c103cccdd9e
SZ.ofc <158851814+sagazdev287@users.noreply.github.com>
2024-05-18 21:50:15 -0300

    Update 01.js
    Manutençao!!!

--- 01.js
+++ 01.js
@@ -1,1 +1,11 @@
+var a;
+a="Site em manutençao, em caso de erros aguarde alguns minutos e tente novamente!!"
+alert(a);
+
+var a;
+a="Término da manutençao previsto para as 22:30 hrs!"
+alert(a);
+
+
+
 console.log("Site monitorado 48H!");
