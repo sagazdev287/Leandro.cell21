ae40aa3845d87e444cb8ccc5e5e63b661c556fa8
SZ.ofc <158851814+sagazdev287@users.noreply.github.com>
2024-05-19 17:12:58 -0300

    Update 01.js

--- 01.js
+++ 01.js
@@ -1,10 +1,10 @@
-var a;
-a="Site em manutenção, em caso de erros aguarde alguns minutos e tente novamente!!"
-alert(a);
+//var a;
+//a="Site em manutenção, em caso de erros aguarde alguns minutos e tente novamente!!"
+//alert(a);
 
-var a;
-a="Término da manutençao previsto para as 22:30 hrs!"
-alert(a);
+//var a;
+//a="Término da manutençao previsto para as 22:30 hrs!"
+//alert(a);
 
 
 
