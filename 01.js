fb3669a15d763af202e5e5587ac3d19e8c483a00
SZ.ofc <158851814+sagazdev287@users.noreply.github.com>
2024-07-31 15:27:14 -0300

    Update 01.js

--- 01.js
+++ 01.js
@@ -1,13 +1,13 @@
-//var a;
-//a="Site em manutenção!"
-//alert(a);
-//var a;
-//a="Término da manutençao previsto para as 01:35 hrs,#Equipesagazprogramer"
-//alert(a);
-var nome = window.prompt('Qual é seu nome ?');
-window.alert('Seja bem-vindo a loja Leandro celulares ' + nome + ', fique avontade!!')
+var a;
+a="Site em manutenção!"
+alert(a);
+var a;
+a="Término da manutençao previsto para as 17:30 hrs,#Equipesagazprogramer"
+alert(a);
+//var nome = window.prompt('Qual é seu nome ?');
+//window.alert('Seja bem-vindo a loja Leandro celulares ' + nome + ', fique avontade!!')
 
 
-console.log("Site monitorado 48H!");
+//console.log("Site monitorado 48H!");
 
 setInterval(() => { let rgb = "rgb(" + Math.ceil(Math.random() * 255) + ", " + Math.ceil(Math.random() * 255) + ", " + Math.ceil(Math.random() * 255) +")"; document.querySelector("nav").style.backgroundColor = rgb }, 1000)
